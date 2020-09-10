3912437713a498763fc6ac73fbdbb85900b60117
togo-max0630 <a.kirigo.t0630@gmail.comgit config --global user.name togo-max0630>
2020-09-10 16:11:37 +0900

    [Java]変数について

--- Chapter02-2.java
+++ Chapter02-2.java
@@ -1,5 +1,6 @@
 public class Chapter02 {
   public static void main(String[] args) {
+      // 処理(文)
     System.out.println("Hello WEBCAMP!");
     System.out.println("Hello Java!");
   }
---
+++ Variable01.java
@@ -0,0 +1,17 @@
+public class Variable01 {
+
+    public static voi d main(String[] args) {
+        int num1;  //int型の変数num1を宣言
+        String str1;  // String型の変数str1を宣言
+        num1 = 10;  // num1を初期化
+        str1 = "Hello World!";  // str1を初期化
+        System.out.println(num1);
+        System.out.println(str1);
+
+        num1 = 20;  // num1に20を再代入
+        System.out.println(num1);
+
+        int num2 = 30;  // int型の変数num2を宣言と同時に初期化
+        System.out.println(num2);
+    }
+}
