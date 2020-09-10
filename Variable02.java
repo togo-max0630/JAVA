c216a96d1967811036d85a0385918fc3ab05d90d
togo-max0630 <a.kirigo.t0630@gmail.comgit config --global user.name togo-max0630>
2020-09-10 23:19:46 +0900

    [java]定数について

---
+++ Variable02.java
@@ -0,0 +1,13 @@
+
+public class Variable02 {
+  
+  public static void main(String[] args) {
+    //定数の宣言
+    final int MIN_NUMBER = 10;
+    System.out.println(MIN_NUMBER);
+    
+    //再代入しようとするとエラー
+    //MIN_NUMBER = 20;
+  }
+
+}
