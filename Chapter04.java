a97579379608f849d3641eb066f2cab33d5ee757
togo-max0630 <a.kirigo.t0630@gmail.comgit config --global user.name togo-max0630>
2020-09-10 23:38:07 +0900

    [Java]変数・定数練習問題

---
+++ Chapter04.java
@@ -0,0 +1,11 @@
+public class Chapter04 {
+  
+  public static void main(String[] args) {
+    String str = "朝田桐伍";
+    final int LUCKY_NUMBER = 7;
+    System.out.println(str);  
+    str = "猫になりたい";
+    System.out.println(str);
+    System.out.println(LUCKY_NUMBER); 
+  }
+}
