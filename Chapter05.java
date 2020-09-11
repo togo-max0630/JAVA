4a783671c339957d4f9c78dfb11eeda4d355eb1d
togo-max0630 <a.kirigo.t0630@gmail.comgit config --global user.name togo-max0630>
2020-09-11 16:19:22 +0900

    [Java]型の基本練習

---
+++ Chapter05.java
@@ -0,0 +1,20 @@
+public class Chapter05 {
+  public static void main(String[] args) {
+    long L = 123456789L;
+    System.out.println(L);
+    double D = 3.14D;
+    System.out.println(D);
+    boolean B = true;
+    System.out.println(B);
+
+    Integer I = 1;
+    System.out.println(I);
+    Float F = 1.11F;
+    System.out.println(F);
+    Character C = 'あ';
+    System.out.println(C);
+
+    int[] array = { 0, 10, 20, 23, 40 };
+    System.out.println(array[3]);
+  }
+}
